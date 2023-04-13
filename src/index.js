ec39dd0c651e2886e6f59150040c6896ee4e8735
iamsh2580 <iamsh2580@gmail.com>
2023-04-13 20:09:04 +0900

    conditionalRendering

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import ConfirmButton from './chapter08/ConfirmButton';
+import LandingPage from './chapter09/LandingPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
     root.render(
     <React.StrictMode>
-     <ConfirmButton/>
+     <LandingPage/>
   </React.StrictMode>
 );
 
